210eb4faef82e36d3e070cec181e35a5bc006f78
Sy TRAORE <61638029+ineditbf@users.noreply.github.com>
2020-03-07 08:54:17 +0000

    master

---
+++ ChallengeProject/DiagnoseMe/Simulation PHP sources/includes/header.php
@@ -0,0 +1,22 @@
+
+			 <link rel="stylesheet" href="css/font-awesome.min.css">
+	<nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php" style="font-size:28px">DiagnoseMe <font style="font-size:18px">(Simulation Only)</font></a>
+		 
+                        <div class="collapse navbar-collapse" id="navbarExample">
+
+
+			
+ 
+
+            </div>
+        </div>
+    </nav>
+
+
+     
+    </nav>
